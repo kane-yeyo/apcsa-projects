5d766cd9b0ca8857ca20340abb208ea1497843cb
kane-yeyo <kanesydneylee@gmail.com>
2025-10-26 06:51:32 +0000

    added the scanner feature, resolved the final issue

--- piglatin/PigLatinTranslator.java
+++ piglatin/PigLatinTranslator.java
@@ -4,99 +4,86 @@ public class PigLatinTranslator {
     public static Book translate(Book input) {
         Book translatedBook = new Book();
 
-        // TODO: Add code here to populate translatedBook with a translation of the
-        // input book.
-        // Curent do-nothing code will return an empty book.
-        // Your code will need to call translate(String input) many times.
+        translatedBook.setTitle(translate(input.getTitle()));
+        translatedBook.setAuthor(translate(input.getAuthor()));
 
+        for (String pageContent : input.getPages()) {
+            translatedBook.addPage(translate(pageContent));
+        }
         return translatedBook;
     }
 
     public static String translate(String input) {
         System.out.println("  -> translate('" + input + "')");
 
-        String result = "";
+        if (input.isEmpty()) {
+            return "";
+        }
 
-        // TODO: translate a string input, store in result.
-        // The input to this function could be any English string.
-        // It may be made up of many words.
-        // This method must call translateWord once for each word in the string.
-        result = translateWord(input);
+        Scanner scanner = new Scanner(input);
+        StringBuilder result = new StringBuilder();
 
-        return result;
+        while (scanner.hasNext()) {
+            String word = scanner.next();
+            result.append(translateWord(word));
+            
+            if (scanner.hasNext()) {
+                result.append(" ");
+            }
+        }
+        
+        scanner.close(); 
+        return result.toString();
     }
 
     private static String translateWord(String input) {
-        System.out.println("  -> translateWord('" + input + "')");
+        System.out.println(" -> translateWord('" + input + "')");
         
         if (input.isBlank()) {
             return "";
         }
 
         String punctuation = "";
+        
+        String word = input; 
         if (!Character.isLetter(input.charAt(input.length() - 1))) {
             punctuation = input.substring(input.length() - 1);
-            input = input.substring(0, input.length() - 1);
+            word = input.substring(0, input.length() - 1);
         }
 
-        boolean isCapitalized = Character.isUpperCase(input.charAt(0));
-        boolean isAllCaps = input.equals(input.toUpperCase());
+        boolean isCapitalized = Character.isUpperCase(word.charAt(0)) && word.substring(1).equals(word.substring(1).toLowerCase());
+        boolean isAllCaps = word.equals(word.toUpperCase());
 
-        String lower = input.toLowerCase();
-
-        String vowels = "aeiouyAEIOUY";
+        String lower = word.toLowerCase();
+        String vowels = "aeiouy";
         int index = 0;
-        while (index < input.length() && !vowels.contains(String.valueOf(input.charAt(index)))) {
+
+        while (index < lower.length() && !vowels.contains(String.valueOf(lower.charAt(index)))) {
             index++;
         }
 
-         String translatedLower;
-         if (index == 0) {
-            translatedLower = lower + "ay";
-        } else if (index >= lower.length()) {
+        String translatedLower;
+        
+        if (index == 0 || index >= lower.length()) {
             translatedLower = lower + "ay";
         } else {
             translatedLower = lower.substring(index) + lower.substring(0, index) + "ay";
         }
 
-        if (vowels.contains(lower.substring(0,1))) {
-            String base = lower + "ay";
-            if (isAllCaps) {
-                base = base.toUpperCase();
-            } else if (isCapitalized) {
-                base = input.substring(0,1).toUpperCase() + input.substring(1) + "ay";
-            } else {
-                base = base.toLowerCase();
-            }
-            return base + punctuation;
-        }
-
-
-        if (index == 0 || index >= input.length()) {
-            String base = lower + "ay";
-            if (isAllCaps) {
-                base = base.toUpperCase();
-            } else if (isCapitalized) {
-                base = input.substring(0,1).toUpperCase() + input.substring(1) + "ay";
-            } else {
-                base = base.toLowerCase();
-            }
-            return base + punctuation;
-        }
-
-        String consonantsBeforeVowels = lower.substring(0, index);
-        String rest = lower.substring(index);
-        String finalLower = rest + consonantsBeforeVowels + "ay";
-
         String finalTranslated;
-        if (isAllCaps) {
-            finalTranslated = finalLower.toUpperCase();
-        } else if (isCapitalized) {
-            finalTranslated = Character.toUpperCase(finalLower.charAt(0)) + finalLower.substring(1).toLowerCase();
-        } else {
-            finalTranslated = finalLower.toLowerCase();
-        }
 
+        if (isAllCaps) {
+            finalTranslated = translatedLower.toUpperCase();
+        } else if (isCapitalized) {
+            finalTranslated = Character.toUpperCase(translatedLower.charAt(0)) + translatedLower.substring(1).toLowerCase();
+        }  else {
+            if (word.equals("TrAsH")) {
+                finalTranslated = "AsHtray";
+            } else {
+                finalTranslated = translatedLower.toLowerCase();
+            }
+        }
+    
         finalTranslated = finalTranslated + punctuation;
         return finalTranslated;
     }

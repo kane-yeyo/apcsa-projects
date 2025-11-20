2dc7cd770d56d359e178831ba54f2d9279e5ac1d
kane-yeyo <kanesydneylee@gmail.com>
2025-11-20 22:20:26 +0000

    began moving code from google docs to github, working with sruti

--- cards/Card.java
+++ cards/Card.java
@@ -1,6 +1,19 @@
 package cards;
 
 public class Card {
+    private int suit;
+    private int value;
 
+    private static String[] SUITS = {"♦", "♣", "♥", "♠"};
+    private static String[] VALUES = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
 
-}
+    public Card(int suit, int value) {
+        if (suit < 0) suit = 0;
+        if (suit > 3) suit = 3;
+        this.suit = suit;
+
+        if (value < 0) value = 0;
+        if (value > 12) value = 12;
+        this.value = value;
+    }
+

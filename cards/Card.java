afb2d8c3a5c0a58522c11998c071a0829caf5d60
kane-yeyo <kanesydneylee@gmail.com>
2025-11-21 02:11:06 +0000

    added comments to the deck class

--- cards/Card.java
+++ cards/Card.java
@@ -4,17 +4,17 @@ public class Card {
     private int suit;
     private int value;
 
-    private static String[] SUITS = {"♦", "♣", "♥", "♠"};
+    private static String[] SUITS = {"♦", "♣", "♥", "♠"}; //copied symbols from online
     private static String[] VALUES = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
 
-    public Card(int suit, int value) {
+    public Card(int suit, int value) { 
         if (suit < 0) suit = 0;
         if (suit > 3) suit = 3;
-        this.suit = suit;
+        this.suit = suit; //0 to 3 since there are 4 elements in SUITS
 
         if (value < 0) value = 0;
         if (value > 12) value = 12;
-        this.value = value;
+        this.value = value; //0 to 12 since there are 13 elements in VALUES
     }
 
     public int getValue() {
--- cards/Deck.java
+++ cards/Deck.java
@@ -1,45 +1,45 @@
 package cards;
 
 public class Deck {
-    private Card[] cards = new Card[52];
-    private int index = 0;
+    private Card[] cards = new Card[52]; //since a standard deck has 52 cards
+    private int index = 0; //keeps track of cards drawn
 
     public Deck() {
-        int k = 0;
-        for (int s = 0; s < 4; s++) {
-            for (int v = 0; v < 13; v++) {
-                cards[k++] = new Card(s, v);
+        int pos = 0; //pos for position
+        for (int s = 0; s < 4; s++) { //s for suit
+            for (int v = 0; v < 13; v++) { //v for value
+                cards[pos++] = new Card(s, v);
             }
         }
     }
 
     public void shuffle() {
         for (int i = 0; i < cards.length; i++) {
-            int r = (int)(Math.random() * cards.length);
-            Card temp = cards[i];
-            cards[i] = cards[r];
-            cards[r] = temp;
+            int r = (int)(Math.random() * cards.length); //chooses a random card from 0 to 51 (since there are 52 total)
+            Card hold = cards[i]; //"hold" since you're temporarily holding onto a card
+            cards[i] = cards[r]; //i, the index, becomes r (random)
+            cards[r] = hold;
         }
-        index = 0;
+        index = 0; //reset to draw from the top again
     }
 
-    public void cut(int n) {
-        Card[] temp = new Card[52];
-        int k = 0;
+    public void cut(int n) { //int n for the number of the card where the cut is at
+        Card[] hold = new Card[52];
+        int pos = 0;
 
-        for (int i = n; i < cards.length; i++) {
-            temp[k++] = cards[i];
+        for (int i = n; i < cards.length; i++) { //moves bottom to top
+            hold[pos++] = cards[i];
         }
-        for (int i = 0; i < n; i++) {
-            temp[k++] = cards[i];
+        for (int i = 0; i < n; i++) { //moves top to bottom
+            hold[pos++] = cards[i];
         }
 
-        cards = temp;
+        cards = hold;
         index = 0;
     }
 
     public Card draw() {
-        if (index >= cards.length) {
+        if (index >= cards.length) { //if more than 52 cards are drawn, there's nothing left
             return null;
         }
         return cards[index++];
@@ -49,6 +49,6 @@ public class Deck {
         for (int i = 0; i < n; i++) {
             System.out.print(cards[i] + " ");
         }
-        System.out.println();
+        System.out.println(); //displays the first n cards
     }
 }

2a184af5876cf36e502ef2966dcac3ec0aa7f11f
kane-yeyo <kanesydneylee@gmail.com>
2026-01-08 22:53:58 +0000

    finished

--- warmup/Main.java
+++ warmup/Main.java
@@ -3,7 +3,14 @@ package warmup;
 public class Main {
 
     public static void main(String[] args) {
-        // Your code here.
+        for (int i = 0; i < 1000; i++) {
+            int sum = 0;
+            if (i%3 == 0 || i%5 == 0) {
+                System.out.println(i);
+                sum += i;
+            }
+            System.out.println(sum);
+        }
 
     }
 }

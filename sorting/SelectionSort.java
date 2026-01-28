49b2ef382af796bb3341d0d5e00a9f50deba1cd3
kane-yeyo <kanesydneylee@gmail.com>
2026-01-28 03:45:06 +0000

    created the second for loop for updating minIndex as new, smaller elements are found

--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -6,6 +6,11 @@ public class SelectionSort implements Sorter {
         System.out.println("Selection Sort!!!");
         for (int i = 0; i < input.length - 1; i++) { //stops before last element since it'll be set to greatest
             int minIndex = i; //assumes first is smallest
+            for (int j = i + 1; j < input.length; j++) { //goes through elements to the right of the smallest
+                if (input[j] < input[minIndex]) { //if the new element is smaller, minIndex is set to it
+                    minIndex = j;
+                }
+            }
             
         }
     }

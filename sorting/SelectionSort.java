953baeb4d73761ea6d03483fac0194822c520dd6
kane-yeyo <kanesydneylee@gmail.com>
2026-01-28 03:40:45 +0000

    created the first for loop for traversing array and assuming the first value is the smallest one

--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -4,5 +4,9 @@ public class SelectionSort implements Sorter {
 
     public void sort(int[] input) {
         System.out.println("Selection Sort!!!");
+        for (int i = 0; i < input.length - 1; i++) { //stops before last element since it'll be set to greatest
+            int minIndex = i; //assumes first is smallest
+            
+        }
     }
 }

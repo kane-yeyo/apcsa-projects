68297789aa5ac56087833d164da639393d1cded9
kane-yeyo <kanesydneylee@gmail.com>
2026-01-28 03:09:52 +0000

    made a for loop to traverse the array and store elements to move them around

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -5,4 +5,11 @@ public class InsertionSort implements Sorter {
     public void sort(int[] input) {
         System.out.println("Insertion Sort!!!");
     }
+
+    public void sort(int[] input) {
+        for (int i = 1; i < input.length; i++) { //starts sort from 2nd element, 1st is considered "sorted"
+            int temp = input[i]; //stores current element
+            int j = i - 1;
+        }
+    }
 }

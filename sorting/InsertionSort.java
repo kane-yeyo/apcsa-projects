f50a8aa1a7b145e12d85836f04778da7d5f631ae
kane-yeyo <kanesydneylee@gmail.com>
2026-01-28 03:49:21 +0000

    adjusted comments

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -5,9 +5,9 @@ public class InsertionSort implements Sorter {
     public void sort(int[] input) {
         System.out.println("Insertion Sort!!!");
         for (int i = 1; i < input.length; i++) { //starts sort from 2nd element, 1st is considered "sorted"
-            int temp = input[i]; //stores current element
+            int temp = input[i]; //stores current value
             
-            int j = i - 1; //takes left element of current one
+            int j = i - 1; //takes left value of current one
             while (j >= 0 && input[j] > temp) { //keeps going if left elements > temp
                 input[j+1] = input[j]; //moves to the right
                 j--; //increments downward
--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -7,12 +7,12 @@ public class SelectionSort implements Sorter {
         for (int i = 0; i < input.length - 1; i++) { //stops before last element since it'll be set to greatest
             int minIndex = i; //assumes first is smallest
             for (int j = i + 1; j < input.length; j++) { //goes through elements to the right of the smallest
-                if (input[j] < input[minIndex]) { //if the new element is smaller, minIndex is set to it
+                if (input[j] < input[minIndex]) { //if the new value is smaller, minIndex is set to it
                     minIndex = j;
                 }
             }
             int temp = input[minIndex]; //saves minimum value in temporary element
-            input[minIndex] = input[i]; //moves value from index i to the spot where the smallest element was
+            input[minIndex] = input[i]; //moves value from index i to the spot where the smallest value was
             input[i] = temp; //smallest value put at position i
         }
     }

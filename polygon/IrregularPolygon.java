753e35c044b39d8cb9bfdaf347843a73fdb66565
kane-yeyo <kanesydneylee@gmail.com>
2026-03-04 06:05:48 +0000

    completed the area method with comments

--- polygon/IrregularPolygon.java
+++ polygon/IrregularPolygon.java
@@ -17,14 +17,35 @@ public class IrregularPolygon {
     }
 
     public double perimeter() {
-        // TODO: Calculate the perimeter.
-        return 3.14;
+        if (myPolygon.size() < 2) { //one point doesn't have a perimeter
+            return 0.0;
+        }
+
+        double perimeter = 0.0;
+        for (int i = 0; i < myPolygon.size(); i++) {
+            Point2D.Double current = myPolygon.get(i); 
+            Point2D.Double next = myPolygon.get((i + 1) % myPolygon.size()); //takes the next point, and the modulus prevents overflow
+
+            perimeter += current.distance(next); //distance function applies the formula to the points
+        }
+        return perimeter;
     }
 
     public double area() {
-        // TODO: Calculate the area.
-        Double area = 0.0;
-        return area;
+        if (myPolygon.size() < 3) { //polygons must have a minimum of 3 sides to be enclosed
+            return 0.0;
+        }
+
+        double area = 0.0; //area is calculated with the shoelace formula as you showed on the slides
+
+        for (int i = 0; i < myPolygon.size(); i++) {
+            Point2D.Double current = myPolygon.get(i);
+            Point2D.Double next = myPolygon.get((i + 1) % myPolygon.size()); //same logic as the perimeter method
+
+            area += (current.getX() * next.getY()); //diagonal downward values are positive
+            area -= (current.getY() * next.getX()); //diagonal upward values are negative
+        }
+        return Math.abs(area) * 0.5; //ensures area is positive and takes half of the value since the formula returns double the area
     }
 
     public void draw()

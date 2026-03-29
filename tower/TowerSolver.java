831097bf82f81525281b2f07958ac7ba946212ea
kane-yeyo <kanesydneylee@gmail.com>
2026-03-29 06:51:05 +0000

    made the private recursive solve method

--- tower/TowerSolver.java
+++ tower/TowerSolver.java
@@ -14,10 +14,15 @@ public class TowerSolver {
         solve(model.getHeight(), 0, 2, 1); //starting tower, ending tower, middle tower
     }
 
-    // Create an overloaded solve(...) method
-    // This new method will be recursive (call itself)
-    //
-    // [ solve method here]
-    //
+    private void solve(int n, int startingTower, int endingTower, int middleTower) {
+        if (n == 0) {
+            return; //no disks to move
+        }
 
+        solve(n-1, startingTower, middleTower, endingTower); //move n-1 disks from the starting tower to the middle tower
+
+        model.move(startingTower, endingTower); //move the nth disk from the starting tower to the ending tower
+
+        solve(n-1, middleTower, endingTower, startingTower); //move n-1 disks from the middle tower to the ending tower
+    }
 }
